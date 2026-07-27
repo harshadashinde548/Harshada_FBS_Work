b965e42a92f53b7275f554d6b8c3820d0233bd07
harshadashinde548 <harshadashinde548@gmail.com>
2026-07-27 22:02:25 +0530

    Add files via upload
    Assignment 6

---
+++ core python/assignment/Assignment6/Q c.py
@@ -0,0 +1,13 @@
+# write a program to follwing patterns
+#
+# 
+#
+#
+for i in range(1,5):
+    k=1
+    for j in range(1,5-i):
+        print(' ',end=' ')
+    for j in range(1,i+1):
+        print(' ',k,end=' ')
+        k=k*(i-j)//j
+    print()        
---
+++ core python/assignment/Assignment6/Q g.py
@@ -0,0 +1,10 @@
+#write a program to follwing patterns
+
+for i in range(1,6):
+    for j in range(1,6-i):
+        print(' ',end=' ')
+    for j in range(1,i+1):
+        print(chr(64+j),end=' ')
+    for j in range(i+1,2*i):
+        print(chr(64+j),end=' ')  
+    print()      

d6992f2f3023a242fc1288bd1f1485881b21bb22
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-05 19:10:07 +0530

    Delete core python/assignment/Assignment7 directory

--- core python/assignment/Assignment7/Q3.py
+++
@@ -1,9 +0,0 @@
-#  write a program to follwing patterns
-
-for i in range(1,6):
-    for j in range(1,i+1):
-        if(i==5 or j==1 or i==j):
-            print(j,end=' ')
-        else:
-            print(' ',end=' ')  
-    print()             

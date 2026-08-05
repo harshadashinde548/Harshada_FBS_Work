1356b4340d2d97fdb045963e2f37a28515f7d646
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-05 19:19:11 +0530

    Add files via upload
    Assignment 7

---
+++ core python/assignment/Assignment7/Q6.py
@@ -0,0 +1,12 @@
+#  write a program to follwing patterns
+for i in range(1,6):
+    for j in range(i,6):
+        if(i==1 or j==5 or i==j):
+            print(j,end=' ')
+        else:
+            print(' ',end=' ')
+    print()        
+                       
+    
+                    
+          

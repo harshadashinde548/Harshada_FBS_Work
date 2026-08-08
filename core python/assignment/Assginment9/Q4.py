cefa2f220d819fc1dd53587fdbc1c550d21ce7cb
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-09 00:17:34 +0530

    Delete core python/assignment/Assginment9 directory

--- core python/assignment/Assginment9/Q4.py
+++
@@ -1,8 +0,0 @@
-def SON(n):
-    if(n>0):
-        return n+SON(n-1)
-    else:
-        return 0
-n=int(input("Enter the number:"))
-res=SON(n)
-print("sum of Number",res)    
--- core python/assignment/Assginment9/Q9.py
+++
@@ -1,7 +0,0 @@
-def power(m,n):
-    return 1
-    return m*power(m,n-1)
-m=int(input("Enter the value of m"))
-n=int(input("Enter the value of n"))
-result=power(m,n)
-print(f"{m}Raised to the power{n}={result}")

d223c3a940b8426ae0189d6d2cd6262313e5b675
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-09 00:20:11 +0530

    Add files via upload
    Assignment 9

---
+++ core python/assignment/Assignment9/Q7.py
@@ -0,0 +1,10 @@
+def SOD(n):
+    if(n>0):
+        d=n%10
+        n=n//10
+        return d+SOD(n)
+    else:
+        return 0
+n=int(input("Enter the number:"))
+res=SOD(n)
+print("Sum of digits",res)

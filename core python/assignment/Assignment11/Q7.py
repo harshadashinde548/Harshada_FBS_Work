7b6c88dd316a8a8c5f53e97a325e047ddf20a69e
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-11 01:14:57 +0530

    Add files via upload
    Assignment 11

---
+++ core python/assignment/Assignment11/Q7.py
@@ -0,0 +1,13 @@
+# 7. Python Program to Find the Intersection of Two Lists
+li1 = [10, 20, 30, 40]
+li2 = [30, 40, 50, 60]
+
+intersection = []
+
+for i in li1:
+    if i in li2:
+        intersection.append(i)
+
+print("List 1 =", li1)
+print("List 2 =", li2)
+print("Intersection =", intersection)

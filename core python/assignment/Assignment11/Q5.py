02995ef567731f887b5646dc3cff6533f0a08a45
harshadashinde548 <harshadashinde548@gmail.com>
2026-08-11 01:15:34 +0530

    Delete core python/assignment/Assignment11 directory

--- core python/assignment/Assignment11/Q5.py
+++
@@ -1,5 +0,0 @@
-# 5. Python Program to Sort a List According to the Length of the Elements
-# within the list.
-li = ["apple", "table", "chair", "cake", "mango"]
-li.sort(key=len)
-print("Sorted List =", li)
